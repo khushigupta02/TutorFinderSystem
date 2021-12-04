9b56be677f69df7886b8dacdae71ee0194ccdb50
Khushi Gupta <84071330+khushigupta02@users.noreply.github.com>
2021-12-04 20:49:38 +0530

    Add files via upload

---
+++ inc/banner.inc.php
@@ -0,0 +1,1 @@
+<a href="index.php"><img src="image/logo.png" style="width:20%; margin-left:-80%; margin-top:1%;"></a>
---
+++ inc/footer.inc.php
@@ -0,0 +1,28 @@
+
+	<div class="container-fluid mx-auto bg-black py-5 footerMain">
+		<div class="row">
+			<div class="col-sm-4 mx-auto">
+				<img src="image/logo.png" class="mx-5" style="width:50%;">
+            </div>
+			<div class="col-sm-4 mx-auto social">
+				<h5 class="font-weight-bold ml-5  text-white">Get Connected</h5>
+				<ul class="list-unstyled d-flex py-2 ">
+					<li class="nav-item mx-3"><a href="https://www.facebook.com/"><i class="fa fa-facebook"></a></i></li>
+					<li class="nav-item mx-3"><a href="https://www.instagram.com/"><i class="fa fa-instagram"></a></i></li>
+					<li class="nav-item mx-3"><a href="https://www.google.com/"><i class="fa fa-google"></a></i></li>
+					<li class="nav-item mx-3"><a href="https://twitter.com/"><i class="fa fa-twitter"></a></i></li>
+					<li class="nav-item mx-3"><a href="https://www.youtube.com/"><i class="fa fa-youtube"></a></i></li>
+				</ul>	
+            </div>
+			<div class="col-sm-4 mx-auto contact">
+			   <h5 class="font-weight-bold ml-5 text-white">Contact</h5>
+			   <ul class="list-unstyled  py-2">
+					<li class="nav-item mx-3 mb-2"><a href=#><i class="fa fa-envelope"><span class="mx-3"> tutorfinder@gmail.com</span></a></i></li>
+					<li class="nav-item mx-3 mb-2"><a href=#><i class="fa fa-phone"><span class="mx-3"> +91-987654321</span></a></i></li>
+					<li class="nav-item mx-3 mb-2"><a href=#><i class="fa fa-map-marker"><span class="mx-3"> Indore,Madhya Pradesh</span></a></i></li>
+				</ul>	
+			   
+            </div>
+        </div>
+		
+	</div>
